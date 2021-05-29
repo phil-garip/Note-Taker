ba89c0da84ef3a3c3f438c4825755b79ee635d10
Philip Garip <phil.garip@gmail.com>
2021-05-29 14:11:47 -0400

    port adjustment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
@@ -58,5 +58,5 @@ app.delete("/api/notes/:id", function(req, res) {
 })
 
 app.listen(port, function() {
-    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+    console.log(`Now listening to port ${PORT}. Enjoy your stay!`);
 })
